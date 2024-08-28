af758ffeacb3b992a75f7d486ce8b1b1f73ec5b2
Jared Lewis <jared@jared.kiwi.nz>
2024-08-28 08:27:42 +0200

    chore: Add pytest

---
+++ tests/test_add.py
@@ -0,0 +1,14 @@
+import pytest
+from two_layer_model._core import add
+
+@pytest.mark.parametrize("a, b, expected", [[1, 2, 3], [2, 3, 5], [1.3, 2.7, 4.0], [1, 2.7, 3.7]])
+def test_add(a, b, expected):
+    assert add(1, 2) == 3
+
+@pytest.mark.parametrize("a", ["1", None])
+def test_add_invalid_types(a):
+    with pytest.raises(TypeError):
+        add(a, 1)
+
+    with pytest.raises(TypeError):
+        add(1, a)
