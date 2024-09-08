4411a71b980bf27fe3b179248383d86a22fc28dd
Jared Lewis <jared@jared.kiwi.nz>
2024-09-08 22:14:41 +0200

    test: Add simple tests

--- rscm-core/src/interpolate/mod.rs
+++ rscm-core/src/interpolate/mod.rs
@@ -1,3 +1,4 @@
+use crate::errors::{RSCMError, RSCMResult};
 use is_close::is_close;
 use num::Float;
 use numpy::ndarray::Array1;
@@ -20,54 +21,56 @@ where
         target: T,
         time_bounds: &Array1<T>,
         allow_extrapolation: bool,
-    ) -> (SegmentOptions, Option<usize>) {
-        let end_segment_idx = time_bounds
-            .as_slice()
-            .unwrap()
-            // Have to use binary_search_by as
-            .binary_search_by(|v| v.partial_cmp(&target).expect("Couldn't compare values"))
-            .unwrap();
+    ) -> RSCMResult<(SegmentOptions, Option<usize>)> {
+        let end_segment_idx = Self::find_segment_index(&target, time_bounds);
 
-        let needs_extrap_forward = (end_segment_idx == time_bounds.len());
+        let needs_extrap_forward = end_segment_idx == time_bounds.len();
         let needs_extrap_backward = !needs_extrap_forward & (end_segment_idx == 0);
 
         // Check if we can fast return because there is an exact match
-        if (!needs_extrap_forward) {
+        if !needs_extrap_forward {
             if is_close!(time_bounds[end_segment_idx], target) {
-                return (SegmentOptions::OnBoundary, Option::from(end_segment_idx));
+                return Ok((SegmentOptions::OnBoundary, Option::from(end_segment_idx)));
             }
         }
 
         let needs_extrap = needs_extrap_backward | needs_extrap_forward;
 
-        if (needs_extrap & (!allow_extrapolation)) {
-            if (needs_extrap_backward) {
-                println!(
-                    "Extrapolation is not allowed and time_target is
-                before the start of the interpolation range.
-                time_target={}
-                start of interpolation range={}",
-                    target, time_bounds[0]
-                );
+        if needs_extrap & (!allow_extrapolation) {
+            if needs_extrap_backward {
+                return Err(RSCMError::ExtrapolationNotAllowed(
+                    target.to_f32().unwrap(),
+                    "start of".to_string(),
+                    time_bounds[0].to_f32().unwrap(),
+                ));
             } else {
-                println!(
-                    "Extrapolation is not allowed and time_target is after the end of the interpolation range.
-                time_target={}
-                end of interpolation range= {}", target,  time_bounds[time_bounds.len()-1]
-                )
+                return Err(RSCMError::ExtrapolationNotAllowed(
+                    target.to_f32().unwrap(),
+                    "end of".to_string(),
+                    time_bounds[time_bounds.len() - 1].to_f32().unwrap(),
+                ));
             }
-            panic!("Extrapolation is not allowed")
         }
         if needs_extrap_backward {
-            (SegmentOptions::ExtrapolateBackward, None)
+            Ok((SegmentOptions::ExtrapolateBackward, None))
         } else if needs_extrap_forward {
-            (SegmentOptions::ExtrapolateForward, None)
+            Ok((SegmentOptions::ExtrapolateForward, None))
         } else {
-            (SegmentOptions::InSegment, Option::from(end_segment_idx))
+            Ok((SegmentOptions::InSegment, Option::from(end_segment_idx)))
         }
     }
 
-    fn interpolate(&self, time_target: T) -> V;
+    fn find_segment_index(target: &T, time_bounds: &Array1<T>) -> usize {
+        let result = time_bounds
+            .as_slice()
+            .unwrap()
+            // Have to use binary_search_by as
+            .binary_search_by(|v| v.partial_cmp(&target).expect("Couldn't compare values"));
+
+        result.unwrap_or_else(|res| res)
+    }
+
+    fn interpolate(&self, time_target: T) -> RSCMResult<V>;
 }
 
 pub struct LinearSpline<'a, T, V> {
@@ -76,18 +79,34 @@ pub struct LinearSpline<'a, T, V> {
     allow_extrapolation: bool,
 }
 
+impl<'a, T, V> LinearSpline<'a, T, V> {
+    pub fn new(time: &'a Array1<T>, y: &'a Array1<V>, allow_extrapolation: bool) -> Self {
+        assert_eq!(time.len(), y.len() + 1);
+
+        Self {
+            time,
+            y,
+            allow_extrapolation,
+        }
+    }
+}
+
 impl<'a, T, V> Interpolate<T, V> for LinearSpline<'a, T, V>
 where
     T: Float + Into<V> + Display,
     V: Float + Into<T>,
 {
-    fn interpolate(&self, time_target: T) -> V {
-        let (segment_options, end_segment_idx) =
-            self.find_segment(time_target, self.time, self.allow_extrapolation);
+    fn interpolate(&self, time_target: T) -> RSCMResult<V> {
+        let segment_info = self.find_segment(time_target, self.time, self.allow_extrapolation);
 
-        if (segment_options == SegmentOptions::OnBoundary) {
+        let (segment_options, end_segment_idx) = match segment_info {
+            Ok(info) => info,
+            Err(e) => return Err(e),
+        };
+
+        if segment_options == SegmentOptions::OnBoundary {
             // Fast return
-            return self.y[end_segment_idx.unwrap()];
+            return Ok(self.y[end_segment_idx.unwrap()]);
         }
 
         let (time1, time2, y1, y2) = match segment_options {
@@ -135,6 +154,48 @@ where
 
         let m = (y2 - y1) / (time2 - time1);
 
-        m * (time_target - time1) + y1
+        Ok(m * (time_target - time1) + y1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use numpy::array;
+    use std::iter::zip;
+
+    #[test]
+    fn test_linear() {
+        let time = array![0.0, 0.5, 1.0, 1.5];
+        let y = array![5.0, 8.0, 9.0];
+
+        let target = vec![0.0, 0.25, 0.5, 0.75, 1.0];
+        let exps = vec![5.0, 6.5, 8.0, 8.5, 9.0];
+
+        let interpolator = LinearSpline::new(&time, &y, false);
+
+        zip(target.into_iter(), exps.into_iter()).for_each(|(t, e)| {
+            println!("target={}, expected={}", t, e);
+            assert!(is_close!(interpolator.interpolate(t).unwrap(), e));
+        })
+    }
+
+    #[test]
+    fn test_linear_extrapolation_error() {
+        let time = array![0.0, 1.0];
+        let y = array![5.0];
+
+        let target = vec![-1.0, -0.01, 1.01, 1.2];
+
+        let interpolator = LinearSpline::new(&time, &y, false);
+
+        target.into_iter().for_each(|t| {
+            println!("target={t}");
+            let res = interpolator.interpolate(t);
+            assert!(res.is_err());
+
+            let err = res.err().unwrap();
+            assert!(err.to_string().starts_with("Extrapolation is not allowed"))
+        })
     }
 }
--- rscm-core/src/lib.rs
+++ rscm-core/src/lib.rs
@@ -6,3 +6,5 @@ pub mod model;
 pub mod python;
 pub mod timeseries;
 pub mod timeseries_collection;
+
+mod errors;
