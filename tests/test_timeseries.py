82333982e12b874dd7f1f53859c4fe5477b65645
Jared Lewis <jared@jared.kiwi.nz>
2024-09-13 18:58:46 +0200

    test: Add test for incorrect type

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 from two_layer_model.core import TimeAxis
 
@@ -9,3 +10,9 @@ def test_timeaxis_create():
 
     npt.assert_allclose(axis.values(), [2000.0, 2020.0, 2040.0])
     npt.assert_allclose(axis.bounds(), [2000.0, 2020.0, 2040.0, 2060.0])
+
+
+def test_timeaxis_create_from_list():
+    match = "'list' object cannot be converted to 'PyArray<T, D>'"
+    with pytest.raises(TypeError, match=match):
+        TimeAxis.from_values([2000.0, 2020.0, 2040.0])
