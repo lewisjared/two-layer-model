5eccda31c1ccbf072da28c2314c8ac073176bd68
Jared Lewis <jared@jared.kiwi.nz>
2024-09-22 22:38:32 +1000

    feat: Add steps for bump and release

--- python/rscm/__init__.py
+++ python/rscm/__init__.py
@@ -4,4 +4,8 @@ Rust Simple Climate Model (RSCM)
 A framework for simple climate models built it Rust.
 """
 
+import importlib.metadata
+
 from ._lib import TwoLayerComponentBuilder  # noqa
+
+__version__ = importlib.metadata.version("rscm")
